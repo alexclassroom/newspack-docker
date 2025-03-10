e0ebea96fd276423f4b622c53673ab86e9c704ba
dkoo <derrick.koo@automattic.com>
2025-03-10 15:42:46 -0600

    chore: plugin architecture

---
+++ plugins/newspack-story-budget/includes/class-initializer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Newspack Hub plugin initialization.
+ *
+ * @package Newspack
+ */
+
+namespace Newspack_Network;
+
+/**
+ * Class to handle the plugin initialization
+ */
+class Initializer {
+	/**
+	 * Runs the initialization.
+	 */
+	public static function init() {
+		// Init the plugin.
+	}
+}
